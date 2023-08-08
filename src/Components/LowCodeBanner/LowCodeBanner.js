4cd7a73cc515a3a7b5cb8ef1ffd2a43d8d2357a8
Targemac <targemac@gmail.com>
2023-08-08 16:23:20 +0100

    created basic structure, not optimized yet

--- src/Components/LowCodeBanner/LowCodeBanner.js
+++ src/Components/LowCodeBanner/LowCodeBanner.js
@@ -1,5 +1,99 @@
+import style from "./style.module.css";
+import {
+  PlaySquareOutlined,
+  GoogleOutlined,
+  GithubOutlined,
+} from "@ant-design/icons";
+
 const LowCodeBanner = () => {
-  return <div>LowCodeBanner LowCodeBanner</div>;
+  const statistics = [
+    {
+      figure: "3.5B/Mo",
+      desc: "Processed Requests",
+    },
+    {
+      figure: "100",
+      desc: "Countries Served",
+    },
+    {
+      figure: "190k",
+      desc: "Developers",
+    },
+  ];
+
+  return (
+    <div className={style.container}>
+      <div className={style.inner}>
+        <div className={style.bigCol}>
+          <div className={style.bigText}>
+            Low-code backend to build modern apps
+          </div>
+
+          <div className={style.smallText}>
+            Store and query relational data on the cloud. Make it accessible
+            over GraphQL and REST with a scalable, open-source backend.
+          </div>
+
+          <div className={style.link}>
+            <a href="/">
+              <PlaySquareOutlined /> How it works
+            </a>
+          </div>
+
+          <div className={style.stat_outter}>
+            <div className={style.stat_title}>Based on the Parse Platform.</div>
+            <div className={style.statistics}>
+              {statistics.map((item) => (
+                <div className={style.item}>
+                  <div className={style.figure}>
+                    <span className={style.plus}>+</span>
+                    {item.figure}
+                  </div>
+                  <div className={style.description}>{item.desc}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className={style.smallCol}>
+          <div className={style.grp1}>
+            <div className={style.text1}>Build your app backend in minutes</div>
+            <div className={style.text2}>No credit card required</div>
+          </div>
+
+          <div className={style.grp2}>
+            <button>
+              <GoogleOutlined /> Sign up with Google
+            </button>
+            <button>
+              <GithubOutlined /> Sign up with GitHub
+            </button>
+          </div>
+
+          <div className={style.grp3}>
+            <div className={style.numb}>or</div>
+          </div>
+
+          <form action="" method="post">
+            <div className={style.grp4}>
+              <input type="email" className="input_grp" placeholder="E-mail" />
+              <input
+                type="password"
+                className="input_grp"
+                placeholder="Password"
+              />
+              <button>Sign up with e-mail</button>
+              <div className={style.legal_statement}>
+                By signing up you agree to our <a href="/">Terms of Service</a>{" "}
+                and <a href="/">Privacy Policy</a>.
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LowCodeBanner;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,166 @@
+import style from "./style.module.css";
+import { CaretUpOutlined } from "@ant-design/icons";
+
 const Navbar = () => {
-  return <div>Navbar Navbar</div>;
+  return (
+    <nav className={style.container}>
+      <div className={style.logo}>
+        <a href="/">bac4App-Clone</a>
+      </div>
+
+      <div className={style.nav_group}>
+        <div className={style.nav_item}>
+          <div className={style.label}>
+            Products
+            <span className={style.icon}>
+              {" "}
+              <CaretUpOutlined />
+            </span>
+          </div>
+          <div className={style.body}>
+            <div className={style.head}>Products</div>
+            <div className={style.body_groups}>
+              <div className={style.group}>
+                <div className={style.group_title}>Backend as a Service</div>
+                <div className={style.group_body}>
+                  Out-of-the-box real-time database, cloud functions,
+                  authentication, APIs (REST/GraphQL), Notifications, and
+                  Blockchain features.
+                </div>
+                <div className={style.group_foot}>
+                  <a href="/">Go to backend</a>
+                </div>
+              </div>
+              <div className={style.group}>
+                <div className={style.group_title}>Container as a Service</div>
+                <div className={style.group_body}>
+                  Deploy your application from a Git repo, monitor with real
+                  time metrics and logs, and scale to deliver the best user
+                  experience.
+                </div>
+                <div className={style.group_foot}>
+                  <a href="/">Go to containers</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={style.nav_item}>
+          <div className={style.label}>
+            Pricing
+            <span className={style.icon}>
+              {" "}
+              <CaretUpOutlined />
+            </span>
+          </div>
+          <div className={style.body}>
+            <div className={style.head}>Products</div>
+            <div className={style.body_groups}>
+              <div className={style.group}>
+                <div className={style.group_title}>Backend as a Service</div>
+                <div className={style.group_body}>
+                  Out-of-the-box real-time database, cloud functions,
+                  authentication, APIs (REST/GraphQL), Notifications, and
+                  Blockchain features.
+                </div>
+                <div className={style.group_foot}>
+                  <a href="/">Go to backend</a>
+                </div>
+              </div>
+              <div className={style.group}>
+                <div className={style.group_title}>Container as a Service</div>
+                <div className={style.group_body}>
+                  Deploy your application from a Git repo, monitor with real
+                  time metrics and logs, and scale to deliver the best user
+                  experience.
+                </div>
+                <div className={style.group_foot}>
+                  <a href="/">Go to containers</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={style.nav_item}>
+          <div className={style.label}>
+            Docs
+            <span className={style.icon}>
+              {" "}
+              <CaretUpOutlined />
+            </span>
+          </div>
+          <div className={style.body}>
+            <div className={style.head}>Products</div>
+            <div className={style.body_groups}>
+              <div className={style.group}>
+                <div className={style.group_title}>Backend as a Service</div>
+                <div className={style.group_body}>
+                  Out-of-the-box real-time database, cloud functions,
+                  authentication, APIs (REST/GraphQL), Notifications, and
+                  Blockchain features.
+                </div>
+                <div className={style.group_foot}>
+                  <a href="/">Go to backend</a>
+                </div>
+              </div>
+              <div className={style.group}>
+                <div className={style.group_title}>Container as a Service</div>
+                <div className={style.group_body}>
+                  Deploy your application from a Git repo, monitor with real
+                  time metrics and logs, and scale to deliver the best user
+                  experience.
+                </div>
+                <div className={style.group_foot}>
+                  <a href="/">Go to containers</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={style.nav_item}>
+          <div className={style.label}>
+            Support
+            <span className={style.icon}>
+              {" "}
+              <CaretUpOutlined />
+            </span>
+          </div>
+          <div className={style.body}>
+            <div className={style.head}>Products</div>
+            <div className={style.body_groups}>
+              <div className={style.group}>
+                <div className={style.group_title}>Backend as a Service</div>
+                <div className={style.group_body}>
+                  Out-of-the-box real-time database, cloud functions,
+                  authentication, APIs (REST/GraphQL), Notifications, and
+                  Blockchain features.
+                </div>
+                <div className={style.group_foot}>
+                  <a href="/">Go to backend</a>
+                </div>
+              </div>
+              <div className={style.group}>
+                <div className={style.group_title}>Container as a Service</div>
+                <div className={style.group_body}>
+                  Deploy your application from a Git repo, monitor with real
+                  time metrics and logs, and scale to deliver the best user
+                  experience.
+                </div>
+                <div className={style.group_foot}>
+                  <a href="/">Go to containers</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={style.action_group}>
+        <button className={style.login_btn}>Login</button>
+        <button className={style.signup_btn}>Signup</button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
